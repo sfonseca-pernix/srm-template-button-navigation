fbe0b97f6b40eafd50687d3fb538b05b37b21d9c
Sergio <sfonseca@estudiantec.cr>
2021-06-10 22:02:53 -0600

    Load info from xml

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-function loadImages(customImageurl){
-  console.log("THIS ID",customImageurl)
-  document.getElementById("loadImage").src = `images/${customImageurl}.png`;
+function loadImages(data){
+  document.getElementById("loadImage").src = `images/${data.src}`;
+  document.getElementById("loadImage").alt = `images/${data.alt}`;
 }
 
-function actionClickButton(elementIdClicked){
-  clickedElement = document.getElementsByClassName('clicked-box')[0]
-  clickedElement.classList.remove('clicked-box')
-  selectedElement = document.getElementById(elementIdClicked);
+function addClickedBoxClass(data){
+  selectedElement = document.getElementById(data.title);
   selectedElement.classList.add("clicked-box");
+}
+
+function actionClickButton(data){
+  clickedElement = document.getElementsByClassName('clicked-box')[0]
+  clickedElement.classList.remove("clicked-box")
+  addClickedBoxClass(data)
 }
 
 function loadDataMobile(option){
@@ -15,21 +19,26 @@ function loadDataMobile(option){
   buttonText.innerHTML = option; 
 }
 
+function loadItemText(data){
+  document.getElementById("itemText").innerHTML = data.text
+}
+
 function clickableItemModel(index,data){
   this.index = index;
-  this.text = data.title;
-  this.img = data.img.src
+  this.title = data.title;
+  this.img = data.img;
+  this.text = data.text;
 }
 
 function ButtonNavigationTemplate() {
 
   this.title = ko.observable();
   this.instruction = ko.observable();
+  this.text = ko.observable();
   this.feedback = ko.observable();
   this.clickableItemsLeft = ko.observableArray([]);
   this.clickableItemsRight = ko.observableArray([]);
-  this.isSubmitted = ko.observable(false);
-  this.isAllCorrect = ko.observable(false);
+  this.selectedClickableItem = ko.observable();
 
   this.scorm = pipwerks.SCORM; // shortcut
 
@@ -46,6 +55,8 @@ function ButtonNavigationTemplate() {
     let data = this.xml2json(xml).dataset;
     this.title(data.title);
     this.instruction(data.instruction);
+    loadImages(data.items.item[0].clickable.img);
+    loadItemText(data.items.item[0].clickable);
 
     let leftItems = [];
     let rightItems = [];
@@ -60,6 +71,14 @@ function ButtonNavigationTemplate() {
 
     this.clickableItemsLeft(leftItems);
     this.clickableItemsRight(rightItems);
+    addClickedBoxClass(data.items.item[0].clickable);
+  }
+
+  this.selectClickableItemHandler = function($data){
+    loadImages($data.img);
+    loadItemText($data);
+    actionClickButton($data)
+    ref.selectedClickableItem($data);
   }
 
   this.init();
@@ -72,19 +91,4 @@ ButtonNavigationTemplate.prototype.constructor = ButtonNavigationTemplate;
 $(document).ready(function () {
   var obj = new ButtonNavigationTemplate();
   ko.applyBindings(obj, $("#button_navigation_template")[0])
-  loadImages("integumentary");
-  loadDataMobile("Integumentary");
-
-  $('.tabButton').click(function(){
-    let elementSelected = this.id
-    loadImages(elementSelected)
-    actionClickButton(elementSelected)
-  });
-
-  $('.click-arrow-right').click(function(){
-    let elementSelected = this.id
-    loadImages(elementSelected)
-    actionClickButton(elementSelected)
-  });
-
 });
